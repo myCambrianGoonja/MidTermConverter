186a42e806484d98ad0def4621729831f242bc0c
gg <A00277328@mycambrian.ca>
2023-07-08 12:53:39 -0400

    set up the layout and view buinding

--- app/src/main/java/com/example/midtermconverter/MainActivity.kt
+++ app/src/main/java/com/example/midtermconverter/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.midtermconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.midtermconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
     }
 }
